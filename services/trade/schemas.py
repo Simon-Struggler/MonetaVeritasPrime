0e7d7b4727e8e3435be32ee8708e0604416b5e57
SimSalobim <105808316+SimSalobim@users.noreply.github.com>
2026-05-22 08:24:12 +0000

    абобо

--- services/trade/schemas.py
+++ services/trade/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, model_serializer
 from datetime import datetime
 from typing import Optional
 from models import TradeStatus
@@ -18,6 +18,20 @@ class TradeOfferResponse(BaseModel):
     created_at: datetime
     updated_at: Optional[datetime]
     model_config = ConfigDict(from_attributes=True)
+
+    @model_serializer
+    def ser(self):
+        return {
+            "id": self.id,
+            "offer_id": self.id,
+            "from_user_id": self.from_user_id,
+            "to_user_id": self.to_user_id,
+            "offered_item_id": self.offered_item_id,
+            "requested_item_id": self.requested_item_id,
+            "status": self.status,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
 
 class TradeAction(BaseModel):
     accept: bool   # true = принять, false = отклонить
